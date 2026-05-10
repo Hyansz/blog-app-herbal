0b3c8b486d2f312558fd53d44ab2b2d1e35f31e1
Raihan Regita Harjuno <raihanregita.ppqita@gmail.com>
2026-05-10 16:50:29 +0700

    delete api

--- app/api/herbs/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    const herbs = await prisma.herb.findMany();
-
-    return NextResponse.json(herbs);
-}
